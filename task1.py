6dbcfc8d870b3fcf05fc042a904f996f0addaf92
arushmish15-png <arushmish.15@gmail.com>
2025-11-12 14:26:55 +0530

    Add files via upload

---
+++ task1.py
@@ -0,0 +1,10 @@
+x = input("Enter a number:")
+y = input("Enter another number:")
+
+print(("Addition:"), int(x) + int(y))
+
+print(("subtraction:"), int(x) - int(y))
+print(("multiplication:"), int(x) * int(y))
+
+
+print(("division:"), int(x) / int(y))
